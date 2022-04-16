593ae7b529cee8a06e557a2e5a4bc3b4a3f5726e
soopeach <hsjeon584@gmail.com>
2022-04-16 20:01:30 +0900

    SimpleNotification

---
+++ app/src/main/java/com/example/notification/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.notification
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import com.example.notification.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    val CHANNEL_ID = "10001"
+    val CHANNEL_NAME = "Soopeach Channel"
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        // 알림 생성
+//        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+//            .setContentTitle("수피치의 알림")
+//            .setContentText("알림이 왔습니다. - 수피치 -")
+//            .setSmallIcon(R.drawable.ic_launcher_foreground)
+//            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//            .build()
+
+        createNotificationChannel()
+
+
+        binding.btn.setOnClickListener {
+            val title = binding.title.text.toString()
+            val content = binding.content.text.toString()
+
+            if (title.isNotEmpty() && content.isNotEmpty()) {
+
+                val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.notify(1, buildNotification(title, content))
+            }else {
+                Toast.makeText(this, "제목과 내용을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+
+
+    }
+
+    // 알림 채널 생성
+    fun createNotificationChannel(){
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply { description = "수피치 알림" }
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+    fun buildNotification(title : String, content : String) : Notification {
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(title)
+            .setContentText(content)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+        return builder
+    }
+
+}
+
+
